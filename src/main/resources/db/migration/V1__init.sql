1b7109f788e7fa28a924fd524d3f34a367f61687
Meganster <ppro100takk@mail.ru>
2018-06-12 02:14:48 +0300

    Perform testing, up performance 4

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -175,27 +175,30 @@ CREATE TRIGGER vote_trigger
 EXECUTE PROCEDURE vote();
 -- --- --
 
-CREATE INDEX new_index_onPost
-  ON Post (thread, parent, id);
+CREATE INDEX New_Posts
+  ON Post (thread, parent, path, id);
 
-CREATE INDEX post_tid_path_id
-  ON Post (thread, id);
+CREATE INDEX Post_threadID_path_id
+  ON Post (thread, path, id);
 
-CREATE INDEX post_threadid_created_id
+CREATE INDEX Post_threadID_created_id
   ON Post (thread, created, id);
 
-CREATE INDEX post_patent_threadid_id
+CREATE INDEX Post_patent_threadID_id
   ON Post (parent, thread, id);
 
-CREATE INDEX thread_forum_created
+CREATE INDEX Thread_forum_created
   ON Thread (forum, created);
 
-CREATE UNIQUE INDEX vote_user_thread
+CREATE UNIQUE INDEX Vote_user_thread
   ON UserVoteForThreads (user_id, thread_id);
 
-CREATE UNIQUE INDEX forum_slug_id
+CREATE INDEX Post_threadID_path
+  ON Post (thread, (path [1]));
+
+CREATE UNIQUE INDEX Forum_slug_id
   ON Forum (slug, id);
 
-CREATE UNIQUE INDEX thread_slug_id
+CREATE UNIQUE INDEX Thread_slug_id
   ON Thread (slug, id);
 
